78976e8ea3c59b78f1dc46f0dd8c477bb4b244ee
jcarlossc <josecscosta@fac.pe.senac.br>
2025-09-24 23:01:29 -0300

    Arquivo jquery adicionado.

---
+++ assets/jquery/jquery.js
@@ -0,0 +1,36 @@
+$(function(){ 
+
+    $(".section-home").show(500); 
+    $(".section-projects").hide(500);
+    $(".section-graphics").hide(500);
+    $(".section-about").hide(500);
+
+    $(".link-home").on("click", function(){
+
+        $(".section-home").show(500);
+        $(".section-projects").hide(500);
+        $(".section-graphics").hide(500);
+        $(".section-about").hide(500);
+    });
+    $(".link-projects").on("click", function(){
+
+        $(".section-home").hide(500);
+        $(".section-projects").show(500);
+        $(".section-graphics").hide(500);
+        $(".section-about").hide(500);
+    });
+    $(".link-graphics").on("click", function(){
+
+        $(".section-home").hide(500);
+        $(".section-projects").hide(500);
+        $(".section-graphics").show(500);
+        $(".section-about").hide(500);
+    });
+    $(".link-about").on("click", function(){
+
+        $(".section-home").hide(500);
+        $(".section-projects").hide(500);
+        $(".section-graphics").hide(500);
+        $(".section-about").show(500);
+    });
+});
